5694e7bb6a1240635409732ef23e9033fddd46c3
Jason Kwong <jasonkwong11@gmail.com>
2017-06-23 19:13:19 -0700

    add 10-second-larargest in BST

---
+++ 10-second_largest_in_binary_tree/index.js
@@ -0,0 +1,36 @@
+function findLargest(rootNode) {
+  if(!rootNode) {
+    throw new Error('Tree must have at least 1 node')
+  }
+
+  if (rootNode.right) {
+    return findLargest(rootNode.right);
+  }
+  return rootNode.value;
+}
+
+function findSecondLargest(rootNode) {
+  if(!rootNode || (!rootNode.left && !rootNode.right)) {
+    throw new Error('Tree must have at least 2 nodes!');
+  }
+
+  //case: we're currently at the largest, and largest
+  // has a left subtree, so 2nd largest is the largest
+  // in said subtree
+
+  if(rootNode.left && !rootNode.right) {
+    return findLargest(rootNode.left);
+  }
+
+  //case: we're at parent of largest and largest
+  //has no left subtree, so 2nd largest must be
+  // current node
+  if (rootNode.right && 
+    !rootNode.right.left &&
+    !rootNode.right.right) {
+    return rootNode.value;
+  }
+  //otherwise: step right
+  return findSecondLargest(rootNode.right);
+}
+
