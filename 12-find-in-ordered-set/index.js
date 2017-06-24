18006058375fdb1ec68668339155ff2f634ae87c
Jason Kwong <jasonkwong11@gmail.com>
2017-06-24 14:36:06 -0700

    binary search q

---
+++ 12-find-in-ordered-set/index.js
@@ -0,0 +1,7 @@
+// Suppose we had an array
+// of nn integers sorted in ascending order. 
+// How quickly could we check if a given integer 
+// is in the array?
+
+// Answer: O(log n) time and O(1) space.
+// Because array is sorted, we can use binary search
