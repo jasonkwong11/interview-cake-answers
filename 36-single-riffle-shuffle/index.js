42d9f0c19a72693a194de80b0e1cd5ea90bdf736
Jason Kwong <jasonkwong11@gmail.com>
2017-06-30 20:39:11 -0700

    implement solution in O(n) time and space

--- 36-single-riffle-shuffle/index.js
+++ 36-single-riffle-shuffle/index.js
@@ -1,29 +1,26 @@
-function isSingleRiffle(half1, half2, shuffledDeck) {
-  //base case
-  if (shuffledDeck.length === 0) {
+
+function isSingleRiffle(half1, half2, shuffledDeck, shuffledDeckIndex, half1Index, half2Index) {
+  shuffledDeckIndex = (typeof shuffledDeckIndex !== 'undefined' ? shuffledDeckIndex : 0);
+  half1Index = (typeof half1Index !== 'undefined') ? half1Index : 0;
+  half2Index = (typeof half2Index !== 'undefined') ? half2Index : 0;
+
+  // base case we've hit the end of shuffledDeck
+  if(shuffledDeckIndex === shuffledDeck.length) {
     return true;
   }
 
-  // if the top of shuffledDeck is the same as the top of
-  // half1 (making sure first that we have a top card in half1)
-
-  if (half1.length && half1[0] === shuffledDeck[0]) {
-
-    // take the top cards off half1 and shuffledDeck
-    // and recurse
-    return isSingleRiffle(half1.slice(1), half2, shuffledDeck.slice(1));
-
-  // if the top of shuffledDeck is the same as the top
-  // of half2
-  } else if (half2.length && half2[0] === shuffledDeck[0]) {
-    // take the top cards off half2 and shuffledDeck and recurse
-    return isSingleRiffle(half1, half2.slice(1), shuffledDeck.slice(1));
-
-  // top of shuffledDeck doesn't match top of half1 or half2
-  // so we know it's not a single riffle
+  // if we still have cards in half1
+  // and the top card of half1 is the same
+  // as the top card of shuffledDeck
+  if ((half1Index < half1.length) &&
+      (half1[half1Index] === shuffledDeck[shuffledDeckIndex])) {
+    half1Index++;
+  } else if ((half2Index < half2.length) && 
+    (half2[half2Index] === shuffledDeck[shuffledDeckIndex])){
+    half2Index++;
   } else {
     return false;
   }
+  shuffledDeckIndex++;
+  return isSingleRiffle(half1, half2, shuffledDeck, shuffledDeckIndex, half1Index, half2Index);
 }
-
-
