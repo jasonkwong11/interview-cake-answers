a9bed42e8985c641b284b3b1334740470b294ea9
Jason Kwong <jasonkwong11@gmail.com>
2017-07-02 19:34:35 -0700

    plan to create random slug generator

--- 44-url-shortener/index.js
+++ 44-url-shortener/index.js
@@ -260,6 +260,433 @@ function redirect(request) {
 // 1. We should be able to store a lot of links.
 // 2. Our shortlinks should be as short as possible.
 
+// Looking at a few examples, we can quickly
+// notice that the more characters we allow
+// in our shortlinks, the more different
+// ShortLinks we can have without making our
+// ShortLinks longer. Specifically, if we 
+// allow c different characters, for n-character-long
+// slugs, we have c^n distinct possibilities.
+
+//How did we get that math?
+// We drew out a few examples and looked for
+// patterns.
+
+// It helps to start out with small numbers.
+// Suppose we only allowed 2 different characters
+// for our slugs 'a' and 'b'. How many possible
+// 1-character slugs would we have?
+// ==> 2: 'a' and 'b'
+
+// How many possible 2-character slugs? Well,
+// we have 2 possibilies for the first char,
+// and for each of those 2 possible first chars
+//, we have 2 possible second characters.
+// That's 2*2 = 4 possibilities overall.
+
+// How many possible 3-char slugs?
+// 2 possibilities for the first char, and for
+// each of those 2 possible first chars, we
+// have 2 possible second chars, and for each
+// of those possible first and second chars,
+// another 2 possible third chars.
+// That's 2*2*2 = 8 possibilities overall.
+
+// ==> generalize it: we have 2^n possible
+// n-character slugs, if we only allow 2
+// possible choices (a and b) for each char
+
+// And if we want to allow c different possible
+// chars, instead of just 2? c^n possiblies 
+// for an n-char-long slug
+
+//>> SO if we're trying to accommodate as 
+// many slugs as possible, we should allow
+// as many chars as we can!
+
+// ... let's do this:
+// 1. figure out the max set of chars we can
+// allow in our random shortlinks
+
+// 2. figure out how many distinct shortlinks
+// we want to accommodate
+
+// 3. figure out how long our shortlinks must
+// be to accommodate that many distinct possibilities
+
+//***NOTE: sketching a process like this before
+// jumping in is hugely impressive. It shows
+// organized, methodical thinking. Whenever
+// you're not sure how to proceed, take a step
+// back and try to write out a process for 
+// getting to the bottom of things. It's fine
+// if you end up straying from your plan-- it'll
+// still help you organize your thinking.
+
+//WHAT CHARACTERS CAN WE ALLOW IN OUR RANDOMLY
+// GENERATED SLUGS?
+
+// What are the constraints on c? Let's think
+// about it:
+
+// 1. we should only use chars that are actually
+// allowed in URLs
+// 2. We should probably only pick chars that
+// are relatively easy to type on a keyboard
+// Remember the use case we talked about where
+// poeple are tyrping in a ShortLink they're 
+// reading off a piece of paper
+
+
+// So what characters are allowed in URLs? It's
+// ok not to know the answer off the top of 
+// your head. but you should be able to 
+// tell your interviewer that you know how to
+// figure it out. Googling or searching on 
+// Stack OVerflow is a fine answer.
+// It's even cooler to say: "I'm sure this is
+// defined in an RFC somewhere"
+
+/*
+RFC = "request for comments". The first ones are 
+form 1969 back in the day of ARPANET, the precursor to the internet.
+
+RFC's define lots of convention for how internet
+communications work.
+
+It turns out the answer is "only alphanumerics,
+the special chars: $-_.+!*'()," and 
+reserved characters used for their reserved
+purpose may be used unencoded within a url.
+Reserved chars with reserved purposes are 
+characters like ? which marks the beginning
+of a query string and # which marks the
+beginning of a fragment/anchor
+  ... we definitely shouldn't use these
+
+So just alphanumerics and the special chars
+are allowed.
+
+On the path portion of a URL, it's case-sensitive.
+  ... so ca.ke/foo and ca.ke/Foo are two different docs
+
+In fact, in keeping with point 2 above about
+ease of typing, lets pull all the special
+characters from our list. It seems like a 
+small lost on character count (8 chars)in
+exchange for a big win on readability and
+typeability. If we find ourselves wanting
+those extra chars, we can add 'em back in
+
+AH BUT WHAT IF a USER wants to specify
+her own slug? she might want to use
+underscores, or dashes, or parentheses
+so let's say for user-specified slugs,
+we allow "$-_.+!*()," still no apostrophe.
+
+Regarding making URLs easy to type:
+  - we might want to consider constraining
+  our character set to clear up common
+  ambiguities. for example: not allowing
+  both uppercase letter O and the number 0.
+  or lowercase letter l and the number 1.
+
+  Font choice can help reduce these ambiguities, 
+  but we don't have any control over the fonts ppl
+  use to display our shortlinks. This is 
+  worthwhile consideration, but at the moment
+  it's adding complexity to a question we're
+  still trying to figure out...
+    ... so lets just mention it and say: "this
+    is something we want to keep an eye on for later
+    but lets put it aside for now." Your interviewer
+    understands thaty you can't accommodate everything
+    in your initial design but she'll appreciate 
+    you showing an ability to anticipate what problems
+    may come up in the user experience.
+
+
+Okay, so with a-z, A-Z, and 0-9, we have
+26 + 26 + 10 = 62 possible chars in our
+randomly-generated slugs. And for user-genned
+slugs, we have another 10 chars for 72 total
+
+HOW MANY DISTINCT SLUGS DO WE NEED?
+
+about how many slugs do we need to be able to 
+accommodate? This is a good question to ask
+your interviewer. She may want you to make
+a reasonable choice yourself. There's no one
+right answer; the mportant thing is to show
+some organized thinking.
+
+
+Here's one way to come up with a ballpark
+estimate: about how many new slugs might we
+create on a busy day? Maybe 100 per minute?
+Hard to imagine more than that. Thats
+100*60*24 about 145 thousand new links a day
+
+52.5 million a year. Whats a number of years
+that feels like 'almost forever' I'd say 100.
+So that's 5.2 trillin slugs. That seems
+sufficiently large. It's pretty dependent
+on the accuracy of our estimate of 100 per
+minute. But it seems to be a pretty reasonable
+ceiling and a purposefully high one.
+
+If we can accommodate that many slugs, we expect
+we'll be able to keep handing out random slugs
+effectively indefinitely.
+
+HOW SHORT CAN WE MAKE OUR SLUGS WHILE STILL
+GETTING ENOUGH DISTINCT POSSIBILITIES???
+
+Let's return to the formula we came up with 
+before: with a c-character-long alphabet
+and slugs of length n, we get c^n possible
+slugs. We want ~5 trillion possible slugs
+
+and we decided on a 62-char alphabet, so
+62^n about equal to 5 trillion. We just
+have to solve for n.
+... we might know that we need to take
+a logarithm to solve for n. but even if
+we know that, this is a tricky thing to
+eyeball
+
+if we're in front of a computer of phone 
+we can just plug it in to wolfram alpha.
+Turns out the answer is about 7.09
+... so 7 chars gets us most of the way to our
+target number of distinct possibilities
+
+It's worth checking how many chars we could
+save by allowign the special chars. 
+so 72^n = 5.2 trillion. we get n is about 6.8
+
+so it's not worth including the special chars
+
+So lets stick with out first instinct to 
+remove those special chars for readability
+purposes and let's choose 7 chars for our slugs
+
+For some added potential brevity, and some 
+added random slugs, we could also allow
+for random slugs with fewer than 7 chars
+
+How many additional random slugs would that
+get us?
+  - If you're a whiz with mathematical series
+  you might know intuitively that the sum of
+  these fewer than 7 character slugs will
+  be far less than the 7 char slugs.
+  We can actually compute this to confirm.
+
+  62^6 (for 6-char slugs) plus 62^5 (for 5-char
+  slugs) ... about 57 billion random slugs.
+  Which isn't that much in comparision to
+  5.2 trillion -- it's two orders of magnitude
+  less. Since this doesn't win us much
+  let's skip it!
+
+  One interesting lesson here: going from
+  6 chars to 7 chars gave us a two orders
+  of magnitude leap in our number of possible
+  slugs. going from 7 chars to 8 should have
+  an even more dramatic effect... so if when
+  we do start running out of 7 char random 
+  slugs, allowing just 1 more char will dramatically
+  push back the point where we run out of random slugs
+
+SO now we know the chars we'll use for slugs
+and we know how many chars we'll use.
+NEXT: HOW do we GENERATE A RANDOM SLUG?
+
+We could just make a random choice for each char:
+
+*/
+
+function getRandom(floor, ceiling) {
+  return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
+}
+
+function generateRandomSlug() {
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var numChars = 7;
+  var result = "";
+
+  for (var i = 0; i < numChars; i++) {
+    var randomIndex = getRandom(0, alphabet.length - 1);
+    result += alphabet[randomIndex];
+  }
+  return result;
+}
+
+/* But how do do we ensure slugs are unique? Two general strategies:
+  1. Re-roll when we hit an already-used slug
+  2. Adjust our slug generation strategy to only ever give us un-claimed slugs.
+
+  If we're serious about our first 2 design goals (short slugs, and accommodating
+  many different slugs), option(2) is clearly better than option(1). 
+  
+  Why? As we have more and more slugs in our database, we'll get more and more
+  collisions. For example, when we're 3/4 of the way through our set of possible
+  7-char slugs, we'd expect to have to make four "rolls" before arriving at
+  a slug that isn't already taken. And it'll just keep going up from there.
+
+  So let's try to come up with a strategy for option (2).
+    ... the answer is base conversion.
+
+  USING BASE CONVERSION TO GENERATE SLUGS:
+  We usually use base-10 numbers, which allow 10 possible numerals:
+  0, 1, 2, 3, 4, 5, 6, ,7 ,8, 9
+
+  Binary is base-2 and has 2 possible numerals: 0 and 1.
+
+  Our random slug alphabet has 62 possible numerals
+  (A-Z, a-z, and 0-9). So we can
+  think of each of our possible "random" slugs as
+   a unique number, ... base-62.
+
+  So let's keep track of a global currentRandomSlugId.
+  When a request for a new random slug comes in, we simply
+  convert that number to base-62 (using our custom numeral set) 
+  and return it. Oh, and we increment the currentRandomSlugId, in
+  preparation for the next request for a random slug.
+*/
+
+var currentRandomSlugId = 0;
+
+function generateRandomSlug() {
+  var newId = currentRandomSlugId++;
+  return baseConversion(newId, base62Alphabet);
+}
+
+// How do we do the base conversion? This is easiest to show by example:
+
+/* 
+Take the number 125 in base 10.
+It has a 1 in the  100s place, a 2 in the 10s place, a 5 in the 1s place.
+In general, the places in a base-10 number are:
+10^0
+10^1
+10^2
+10^3
+etc.
+
+The places in a base-62 number are:
+
+62^0
+62^1
+62^2
+62^3
+etc.
+
+So to convert 125 to base-62, we distribute that 125 across these
+base-62 'places'. The highest 'place' that can take some is 62^1, which
+is 62. 125/62 is 2 with a remainder of 1. So we put a 2 in
+the 62's place and a 1 in the 1's place. So our answer is 21.
+
+What about a higher number --say 7912?
+
+Now we have enough to put something in the 3844's the 62^2 place.
+7912/3844 is 2 with a remainder of 224. So we put a 2 in the 
+3844's place and we distribute that remaining 244 across the 
+remaining places -- the 62's place and the 1's place.
+
+244 / 62 is 3 with a remainder of 38. So we put a 3 in the 62s
+place and a 38 in the 1's place. We have this three-digit number:
+2 3 38.
+  ... now that 38 represents one numeral in our base-62 number
+  so we need to convert that 38 into a specific choice from our 
+  set of numerals: a-z, A-Z, and 0-9.
+
+  Let's number each of our 62 numerals like so:
+
+0: 0,
+1: 1,
+2: 2,
+3: 3,
+...
+10: a,
+11: b,
+12: c,
+...
+36: A,
+37: B,
+38: C,
+...
+62: Z
+
+
+As you can see, our 38th numeral is C. So we convert that 38 into a C. That
+gives us 23C.
+
+One potential issue: the currentRandomSlugId could give us something that a 
+user has already claimed as a user-generated slug. We need to check for that
+and if it happens we'll just increment the currentRandomSlugId and try again
+(and again, potentially, until we hit a 'random' slug that hasn't been used yet)
+*/
+
+var currentRandomSlugId = 0;
+
+function generateRandomSlug() {
+  var slug = "";
+  while(1) {
+    var newId = currentRandomSlugId++;
+    slug = baseConversion(newId, base62Alphabet);
+
+    //make sure slug isn't already used:
+    if(!DB.checkSlugExists(slug)) {
+      break;
+    }
+  }
+  return slug;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
