09ef3b7fededce95ad6e59c2a6ff8a53fe55adfd
Jason Kwong <jasonkwong11@gmail.com>
2017-07-09 19:10:19 -0700

    implement greedy approach for array

---
+++ 01-apple-stocks/index2.js
@@ -0,0 +1,31 @@
+var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
+
+function getMaxProfit(stockPricesYesterday) {
+
+  //check edge case: make sure there's at least 2 prices
+  if (stockPricesYesterday.length < 2) {
+    throw new Error("You need at least 2 prices")
+  }
+  // initialize a currentMin
+  // initialize a currentMax
+
+  // iterate through the stockPrices
+    // for each price, check to see if the 
+    // current price would yield a new Max
+  var currentMin = stockPricesYesterday[0];
+  var maxProfit = stockPricesYesterday[1] - stockPricesYesterday[0];
+
+  for (var i = 0; i <= stockPricesYesterday.length - 1; i++) {
+    let currentPrice = stockPricesYesterday[i]
+    let potentialProfit = currentPrice - currentMin;
+
+    maxProfit = Math.max(maxProfit, potentialProfit);
+    currentMin = Math.min(currentMin, currentPrice)
+  }
+  return maxProfit;
+}
+// Time Complexity:  O(n)... n is the number of stock prices
+// Space Complexity: O(1)... constant space, constant number of variables
+
+// returns 6 (buying for 5 and selling for 11)
+console.log(getMaxProfit(stockPricesYesterday));
