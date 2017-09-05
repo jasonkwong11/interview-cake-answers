8fe1688a4690941beccec6fc80642fca3074a9d2
Jason Kwong <jasonkwong11@gmail.com>
2017-09-05 13:33:45 -0700

    refactor

--- 01-apple-stocks/stock_prices.rb
+++ 01-apple-stocks/stock_prices.rb
@@ -1,20 +1,22 @@
-
 def get_max_profit(stock_prices)
-  raise StandardError("Need at least 2 prices") if stock_prices.length < 2
-  #initialize current highest price
+  raise IndexError if stock_prices.length <= 2
+
+  current_max_profit = stock_prices[1] - stock_prices[0]
+
   current_min = stock_prices[0]
-  #initialize current_max_profit
-  current_max_profit = stock_prices[0] - stock_prices[1]
+ 
+  stock_prices.each_with_index do |price, index|
 
-  stock_prices.each do |price|
-    potential_profit = price - current_min
+    next if index == 0
 
-    current_max_profit = potential_profit if potential_profit > current_max_profit
+    potential_max_profit = price - current_min
 
+    current_max_profit = potential_max_profit if potential_max_profit > current_max_profit
     current_min = price if price < current_min
   end
- puts current_max_profit
+  current_max_profit
 end
 
-stock_prices = [10, 7, 5, 8, 11, 9]
-get_max_profit(stock_prices)
+
+stock_prices = [2, 23, 5, 4, 99, 9]
+puts get_max_profit(stock_prices)
