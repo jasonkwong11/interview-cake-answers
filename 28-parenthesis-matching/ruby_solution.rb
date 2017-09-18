36fb540bdc2f7efd78c8917472b073c2c0ef389f
Jason Kwong <jasonkwong11@gmail.com>
2017-09-17 22:25:41 -0400

    add ruby solution

---
+++ 28-parenthesis-matching/ruby_solution.rb
@@ -0,0 +1,16 @@
+
+
+def get_closing_paren(sentence, opening_paren_index)
+  open_nested_parens = 0
+
+  (opening_paren_index + 1).upto(sentence.length - 1) do |position|
+    char = sentence[position]
+
+    if char == '('
+      open_nested_parens += 1
+    elsif char == ')'
+      open_nested_parens == 0 ? return position : open_nested_parens -= 1
+    end
+  end 
+  raise Exception, "No closing parenthesis!"
+end
